d0d6108f23ecbce949e1cf3a2b1fe955b6abb305
HHS-ChristiaandeHaan <18127371@student.hhs.nl>
2023-12-08 00:45:09 +0100

    Added bootstrap

---
+++ wpr-gr-i/src/bedrijvenportaal/bedrijvenportaal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "../App";
+import NavMenu from "../Components/Header";
+
+function BedrijvenPortaal(){
+    return(
+        App()
+    )
+}
+export default BedrijvenPortaal;
