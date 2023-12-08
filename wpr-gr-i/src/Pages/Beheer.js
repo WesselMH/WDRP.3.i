a447a4512e9f26a400319d34b3ecdd76f2291837
Robin <21104964@student.hhs.nl>
2023-12-08 11:39:03 +0100

    BeheerPortaal begin

---
+++ wpr-gr-i/src/Pages/Beheer.js
@@ -0,0 +1,18 @@
+import Header from '../Components/Header.js'
+// import './Header.js'
+import './Beheer.css'
+
+
+function handleClick(i){
+    console.log(i)
+}
+
+function Beheer({}){
+    return (
+        <>
+            <Header Titel={"Test"} Knop1={"aasdflj"} Knop2={"tweedeknop"}></Header>
+        </>
+    )
+
+}
+export default BP;
