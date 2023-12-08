071e6e5d88a81d7d8714ae49936d0d6d474cb08e
WesselMH <93765122+WesselMH@users.noreply.github.com>
2023-12-08 11:50:07 +0100

    Update App.js
    niet nodig

--- wpr-gr-i/src/App.js
+++ wpr-gr-i/src/App.js
@@ -9,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <br></br>
         <a
           className="App-link"
           href="https://reactjs.org"
