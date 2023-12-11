b6f5cb04070f7696adfde71b2578596dbd2faf59
Lirandy <lnkoeiman@gmail.com>
2023-12-11 11:01:14 +0100

    onnodige dingen uit de app gehaald

--- wpr-gr-i/src/App.js
+++ wpr-gr-i/src/App.js
@@ -1,33 +1,13 @@
 
 import React from 'react';
 import './App.css';
-import background from './backgroundWithGradient.png'
+import VoorPagina from './Pages/VoorPagina'
+
 
 
 function App() {
   return (
-<div className="App" style={{backgroundImage: `url(${background})`}}>
-      <style>
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,200&family=Roboto:ital,wght@1,300&display=swap');
-</style>
-      
-      <nav className='navbar'>
-      
-        <a href="/#">Bedrijvenportaal</a>
-        <a href="/#">Beheerdersportaal</a>
-      </nav>
-
-      <h1 className="titel">Een beter online bestaan begint hier</h1>
-      <h4 className="">Door mee te doen aan verschillende onderzoeken, helpt u ons met het begrijpen van uw beperking en kunnen wij ervoor zorgen dat het internet toegankelijker wordt voor u.</h4>
-
-      <div className="button-container">
-        <button type="button" className='Button'>Login</button>
-        <button type="button" className='Button'>Registreren</button>
-      </div>
-      
-    </div>
-  
-
+      <VoorPagina></VoorPagina>
   );
 }
 
