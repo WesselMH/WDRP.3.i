1cb0287dc0f23b61251d29d0759192d923f6ee6c
HHS-ChristiaandeHaan <18127371@student.hhs.nl>
2023-12-15 11:12:48 +0100

    fixed CSS of bedrijvenportaal.jsx

--- wpr-gr-i/src/Pages/bedrijvenportaal.jsx
+++ wpr-gr-i/src/Pages/bedrijvenportaal.jsx
@@ -4,33 +4,37 @@ import "./bedrijvenportaal.css";
 import background from "./backgroundWithGradient.png";
 
 const opdracht = [
-    { title: 'Opdracht 1', bedrijf:'Mediamarkt', id: 1 },
-    { title: 'Opdracht 2', bedrijf:'Microsoft', id: 2 },
-    { title: 'Opdracht 3', bedrijf:'Albert Heijn', id: 3 },
-    { title: 'Opdracht 4', bedrijf:'Albert Heijn', id: 4 },
-    { title: 'Opdracht 5', bedrijf:'Albert Heijn', id: 5 },
-    { title: 'Opdracht 6', bedrijf:'Albert Heijn', id: 6 },
-  ];
+  { title: 'Opdracht 1', bedrijf: 'Mediamarkt', id: 1 },
+  { title: 'Opdracht 2', bedrijf: 'Microsoft', id: 2 },
+  { title: 'Opdracht 3', bedrijf: 'Albert Heijn', id: 3 },
+  { title: 'Opdracht 4', bedrijf: 'Albert Heijn', id: 4 },
+  { title: 'Opdracht 5', bedrijf: 'Albert Heijn', id: 5 },
+  { title: 'Opdracht 6', bedrijf: 'Albert Heijn', id: 6 },
+  { title: 'Opdracht 7', bedrijf: 'Albert Heijn', id: 7 },
+  { title: 'Opdracht 8', bedrijf: 'Albert Heijn', id: 8 },
+  { title: 'Opdracht 9', bedrijf: 'Albert Heijn', id: 9 },
+  { title: 'Opdracht 10', bedrijf: 'Albert Heijn', id: 10 },
+];
 
-  const listOpdrachten = opdracht.map(opdracht =>
+const listOpdrachten = opdracht.map(opdracht =>
+  <a href="/#">
     <li key={opdracht.id}>
-      <a href="/#">
       <strong>{opdracht.title}</strong>
       <p>{opdracht.bedrijf}</p>
-      </a>
     </li>
-  );  
+  </a>
+);
 
 function BedrijvenPortaal() {
-    return (
-        <div className="bedrijvenportaal" style={{ backgroundImage: `url(${background})` }}>
-            <Header Titel={"Bedrijvenportaal"} Knop1={"Profiel Updaten"} Knop2={"Opdracht Plaatsen"} />
-            <ul className="listOpdracht">
-              <h2>Open Opdrachten</h2>
-              {listOpdrachten}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="bedrijvenportaal" style={{ backgroundImage: `url(${background})` }}>
+      <Header Titel={"Bedrijvenportaal"} Knop1={"Profiel Updaten"} Knop2={"Opdracht Plaatsen"} />
+      <ul className="listOpdracht">
+        <h2>Open Opdrachten</h2>
+        {listOpdrachten}
+      </ul>
+    </div>
+  )
 }
 
 
