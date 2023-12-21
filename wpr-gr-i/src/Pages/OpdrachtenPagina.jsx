97c7437b40e69f54ecf30ab20880e9c6ab809b4e
HHS-ChristiaandeHaan <18127371@student.hhs.nl>
2023-12-21 12:39:49 +0100

    Opdrachtpagina gemaakt volgens nieuw ontwerp

--- wpr-gr-i/src/Pages/OpdrachtenPagina.jsx
+++ wpr-gr-i/src/Pages/OpdrachtenPagina.jsx
@@ -1,33 +1,38 @@
 import React from "react";
 import "./OpdrachtenPagina.css";
-import background from "./backgroundWithGradient.png";
+import background from "../achtergrondfoto.jpg";
 import Header from "../Components/Header";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const opdracht = [
-  { title: 'Opdracht 1', bedrijf: 'Mediamarkt', beschrijving: "Een internet interview over ervaring", datum: "12-12-12", id: 1 },
-  { title: 'Opdracht 2', bedrijf: 'Microsoft', beschrijving: "Een internet interview over ervaring", datum: "12-12-12", id: 2 },
-  { title: 'Opdracht 3', bedrijf: 'Albert Heijn', beschrijving: "Een internet interview over ervaring", datum: "12-12-12", id: 3 },
-  { title: 'Opdracht 4', bedrijf: 'Albert Heijn', beschrijving: "Een internet interview over ervaring", datum: "12-12-12", id: 4 },
+  { title: 'Toegankelijkheidsbeoordeling 2023', bedrijf: 'Mediamarkt', beschrijving: "Deze enquête is ontworpen om inzicht te krijgen in uw ervaringen met de toegankelijkheid van onze producten/diensten/ruimtes. Neem alstublieft de tijd om de vragen eerlijk en gedetailleerd te beantwoorden. Uw input zal direct bijdragen aan het verbeteren van onze inspanningen op het gebied van inclusiviteit.", datum: "12-12-12", id: 1 },
+  { title: 'Inclusie en Welzijn bij Mensen met Beperkingen', bedrijf: 'Bartiméus', beschrijving: "Onderzoek naar Levenskwaliteit bij Personen met een Beperking: Een analyse van sociale inclusie en dagelijkse uitdagingen, met het doel bij te dragen aan verbeterde maatschappelijke participatie en welzijn.", datum: "12-12-12", id: 2 },
+  { title: 'Toegankelijkheid voor Mensen met Beperkingen', bedrijf: 'Accessibility Foundation', beschrijving: "Studie naar Kwaliteit van Leven bij Mensen met Beperkingen: Een diepgaand onderzoek naar de dagelijkse hindernissen en mogelijkheden tot verbetering. Het doel is waardevolle inzichten te vergaren ter bevordering van levenskwaliteit en inclusie voor deze individuen.", datum: "12-12-12", id: 3 },
+  { title: 'Multimedia Toegankelijkheidsbeoordeling 2023', bedrijf: 'Albert Heijn', beschrijving: "Een internet interview om inzicht te krijgen in uw ervaringen met de toegankelijkheid van onze multimediacontent, platforms en services. ", datum: "12-12-12", id: 4 },
   { title: 'Opdracht 5', bedrijf: 'Albert Heijn', beschrijving: "Een internet interview over ervaring", datum: "12-12-12", id: 5 },
   { title: 'Opdracht 6', bedrijf: 'Albert Heijn', beschrijving: "Een internet interview over ervaring", datum: "12-12-12", id: 6 },
   { title: 'Opdracht 7', bedrijf: 'Albert Heijn', beschrijving: "Een internet interview over ervaring", datum: "12-12-12", id: 7 },
   { title: 'Opdracht 8', bedrijf: 'Albert Heijn', beschrijving: "Een internet interview over ervaring", datum: "12-12-12", id: 8 }
-
 ];
 
 const listOpdrachten = opdracht.map((opdracht) => (
-  <Link to={opdracht.href}>
-    <li key={opdracht.id}>
-      <h3>{opdracht.title}</h3>
-      <p>{opdracht.bedrijf}</p>
-      <p>{opdracht.beschrijving}</p>
-      <p>{opdracht.datum}</p>
-    </li>
+  <Link to={opdracht.href} style={{ textDecoration: 'none' }}>
+    <div className="opdracht">
+      <li key={opdracht.id}>
+        <div className="opdrachtHeader">
+          <h3>{opdracht.title}</h3>
+          <p>{opdracht.bedrijf}</p>
+        </div>
+        <div className="opdrachtBescrijving">
+          <p>{opdracht.beschrijving}</p>
+        </div>
+      </li>
+    </div>
   </Link>
 ));
 
 let buttons = [
+  { Naam: "Beschikbare Opdrachten", href: "/" },
   { Naam: "Uitloggen", href: "/" },
 ];
 
@@ -35,16 +40,12 @@ function OpdrachtenPagina() {
   return (
     <div>
       <Header Titel={"Opdrachten"} Knoppen={buttons} />
-      <div className="OpdrachtenPagina" style={{ backgroundImage: `url(${background})` }}>
-        <ul className="listOpdracht">
-          <div className="listOpdrachtHeader">
-            <strong>Titel</strong>
-            <strong>Bedrijf</strong>
-            <strong>Beschrijving</strong>
-            <strong>Datum gepost</strong>
-          </div>
-          {listOpdrachten}
-        </ul>
+      <div>
+        <div className="OpdrachtenPagina" style={{ backgroundImage: `url(${background})` }}>
+          <ul className="listOpdrachten">
+            {listOpdrachten}
+          </ul>
+        </div>
       </div>
     </div>
   );
