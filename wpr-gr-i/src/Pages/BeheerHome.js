e744c7efe62f913c5726a392e4fae52466b4f847
Robin <21104964@student.hhs.nl>
2023-12-22 10:18:36 +0100

    Kleine dingen

--- wpr-gr-i/src/Pages/BeheerHome.js
+++ wpr-gr-i/src/Pages/BeheerHome.js
@@ -1,25 +1,22 @@
-import Header from '../../Components/Header.jsx'
+import Header from '../Components/Header.jsx'
 import './Beheer.css'
-import Knop from '../../Components/Button.jsx'
+import Knop from '../Components/Button.jsx'
 // import { DeskundigePage } from './BeheerDeskundige'
-import { Route, Routes } from 'react-router-dom'
+
 
 
 const Knoppen = [
     {Naam: 'Opdrachten', href:"WesselIsStom"},
-    {Naam: 'Uitloggen', href:"sf"}
+    {Naam: 'Uitloggen', href:"/"}
 ]
 
-function Paginas({}){
-
-}
 
 function Beheer({}){
     return (
         <>
             <Header Titel={"Beheerderspagina"} Knoppen={Knoppen}></Header>
             <div className='Blokjes'>                
-                <Knop  label={"Overzicht van ervaringsdeskundigen"} href={"asdf"} />
+                <Knop  label={"Overzicht van ervaringsdeskundigen"} href={"Beheerdersportaal/BeheerBedrijven"} />
                 <Knop  label={"Overzicht van bedrijven"} href={""} />
                 <Knop  label={"Overzicht van opdrachten"} href={"asdf"} />
             </div>
