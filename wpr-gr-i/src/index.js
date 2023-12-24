530b93bb8cd30fa5cf29f39c46685fb84d779c9e
WesselMH <w.m.horsthuis@live.nl>
2023-12-24 11:42:29 +0100

    laat maar

--- wpr-gr-i/src/index.js
+++ wpr-gr-i/src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import googleLogin from "./google4b7307288f60d226.html"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {googleLogin}
     <App />
   </React.StrictMode>
 );
