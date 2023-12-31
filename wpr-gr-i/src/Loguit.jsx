52a3b3f7a2e576eef1cdd6acb92f296a42e6fdb2
WesselMH <w.m.horsthuis@live.nl>
2023-12-31 17:14:25 +0100

    .

--- wpr-gr-i/src/Loguit.jsx
+++ wpr-gr-i/src/Loguit.jsx
@@ -4,6 +4,8 @@ function Loguit (){
     sessionStorage.setItem("authenticated", false);
     sessionStorage.setItem("role", null);
     sessionStorage.setItem("exp", null);
+    sessionStorage.setItem("userName", null);
+    sessionStorage.setItem("id", null);
 }
 
 export default Loguit;
