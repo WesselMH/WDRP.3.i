b098dcb50e639a18dd82523f0ff3377c27e44b7e
Robin <21104964@student.hhs.nl>
2024-01-24 13:07:37 +0100

    Chat dingen gemaakt

--- wpr-gr-i/src/Pages/Chat.jsx
+++ wpr-gr-i/src/Pages/Chat.jsx
@@ -1,117 +1,47 @@
 import React, { useEffect, useState } from "react";
 import Header from "../Components/Header";
 import "./Chat.css";
-import axios from 'axios';
-// import { Link, Navigate } from "react-router-dom";
-// import background from "./backgroundWithGradient.png";
-// import "./HomePortaal.css";
+import axios from "axios";
 
 const Knoppen = [];
 
-const ChatBerichten = [
-  {
-    gebruiker1: "Lirandy",
-    gebruiker2: "Robin",
-    Bericht: "Test",
-    datum: "11-1-2024",
-  },
-  {
-    gebruiker1: "Robin",
-    gebruiker2: "Wesselweside",
-    Bericht: "Test2",
-    datum: "11-1-2024",
-  },
-  {
-    gebruiker1: "Robin",
-    gebruiker2: "Wesselweside",
-    Bericht: "Test4",
-    datum: "11-1-2024",
-  },
-  {
-    gebruiker1: "Robin",
-    gebruiker2: "Wesselweside",
-    Bericht: "Testawer",
-    datum: "11-1-2024",
-  },
-  {
-    gebruiker1: "Robin",
-    gebruiker2: "Wesselweside",
-    Bericht: "Tesasdfasdfkjhaskcht",
-    datum: "11-1-2024",
-  },
-];
-
-
-
-function Gesprekken(gebruiker1) {
-  const [Gespr, setGespr] = useState("");
-  for (const gesprek in ChatBerichten) {
-    console.log(gesprek.gebruiker1);
-    if (sessionStorage.getItem("userName") === gesprek.gebruiker1) {
-      Gespr.push({ gebruiker2: gesprek.gebruiker2 });
-    }
-    if (sessionStorage.getItem("userName") === gesprek.gebruiker2) {
-      Gespr.push({ gebruiker2: gesprek.gebruiker1 });
-    }
-  }
-  return Gespr;
-}
-
-function ChatComponent() {
-  const [chats, setChats] = useState(ChatBerichten);
-
-  function NieuwChatBericht({ gebruiker1, gebruiker2, Bericht }) {
-    setChats([chats, { gebruiker1, gebruiker2, Bericht }]);
-  }
-  return (
-    <div>
-      Hallo
-      <input></input>
-    </div>
-  );
-}
-
-function ChatPersonen({}) {}
-
-function Verstuur({}) {
-  const [Verstuur, setVerstuur] = useState("");
-}
-
 function Chat() {
   const [Tekst, setTekst] = useState();
-  const [Bericht, setBericht] = useState(ChatBerichten);
-  const [Gebruiker2, setGebruiker2] = useState("");
-  sessionStorage.setItem("userName", "Robin");
-  const berichtToevoegen = ChatBerichten;
+  const [Gesprek, setGesprek] = useState([]);
+  const [Allegesp, setAllegesp] = useState([]);
+  const [Verzender, setVerzender] = useState();
+  const [Ontvanger, setOntvanger] = useState();
+  // sessionStorage.setItem("userName", "Robin"); //-------------------------LET OP
+  const id = "";
+  // setVerzender(sessionStorage.getItem("token"));
+  // const berichtToevoegen = ChatBerichten;
 
-  const GoedeBerichten = () => {
-    return (
-      <>
-        
+  // async function NieuweChat(Tekst) {
+  //   await axios.post("http://localhost:5155/api/Chat", {
+  //     id,
+  //     Verzender,
+  //     Ontvanger,
+  //     Tekst,
+  //   });
+  // }
 
-
-        {/* {Bericht.map((item) => {
-
-          if (
-            (item.gebruiker1 === sessionStorage.getItem("userName")) &
-            (item.gebruiker2 === Gebruiker2)
-          ) {
-            return <div className="VerstuurdBericht">{item.Bericht}</div>;
-          }
-          if (
-            (item.gebruiker2 === sessionStorage.getItem("userName")) &
-            (item.gebruiker1 === Gebruiker2)
-          ) {
-            return <div className="OntvangenBericht">{item.Bericht}</div>;
-          }
-        })} */}
-      </>
-    );
-  };
   useEffect(() => {
-    // GoedeBerichten();
-    console.log(Bericht);
-  }, [Bericht]);
+    const ChatGesprekken = async () => {
+      try {
+        const response = await axios.get("http://localhost:5155/api/Chat", {
+          headers: {
+            Authorization: 'Bearer ${sessionStorage.getItem("token")}',
+          },
+        });
+        console.log(response.data);
+        setGesprek(response.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    ChatGesprekken();
+  }, []);
+
   return (
     <>
       <Header
@@ -120,31 +50,21 @@ function Chat() {
       ></Header>
       <div style={{ height: "79vh" }}>
         <div className="GesprekkenLijst">
-          <button className="Gesprek">
-            {Gesprekken('sessionStorage.getItem("userName")')}
-          </button>
+          {/* {Allegesp.map((gesp) => ( */}
+          {/* <div>{gesp.andere}</div> */}
+          {/* ))} */}
+          <button className="Gesprek"></button>
         </div>
+        {Gesprek.map((gesp) => (
+          <div>
+            <div>Verstuurder: {gesp.verzender}</div>
+            <div>Ontvanger: {gesp.ontvangerId}</div>
+            <div>Bericht: {gesp.tekst}</div>
+          </div>
+        ))}
 
-        <div className="BerichtenBox">
-          <GoedeBerichten />
-        </div>
-
-        <input value={Tekst} onChange={(e) => setTekst(e.target.value)} />
-        <button
-          onClick={() => {
-            setBericht([
-              ...Bericht,
-              {
-                gebruiker1: sessionStorage.getItem("userName"),
-                gebruiker2: "Wessel",
-                Bericht: Tekst,
-              },
-              setTekst(""),
-            ]);
-          }}
-        >
-          Hallo
-        </button>
+        <p>Hallo: </p>
+        {/* <input value={Tekst} onChange={(e) => setTekst(e.target.value)} /> */}
       </div>
     </>
   );
